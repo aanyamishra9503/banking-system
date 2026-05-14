0b03fae72559a875018ad3b8a600b58213d54e25
aanyamishra9503 <aa.nyamishra9503@gmail.com>
2026-05-14 22:06:24 +0530

    first commit

---
+++ account.py
@@ -0,0 +1,9 @@
+#create_account()
+#get_account_details()
+#check_balance()
+#update_balance()
+#delete_account()
+def create_account():
+    name = input("Enter name: ")
+    pin = input("Enter PIN: ")
+    balance = float(input("Enter initial balance: "))
