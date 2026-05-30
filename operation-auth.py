fca304515440ff70558a376c1381e4dd4dae0508
Aanya Mishra <aa.nyamishra9503@gmail.com>
2026-05-30 23:28:42 +0530

    password hashing .py

---
+++ operation-auth.py
@@ -0,0 +1,7 @@
+#for password security 
+'''import hashlib
+   def hash_password(password):
+        return hashlib.sha256(password.encode(*).hexdigest()
+
+   def verify_password(input_pass, stored_hash):
+        return hash_password(input_pass)== stored_hash
