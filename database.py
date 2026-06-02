b2c4e1908cf688ea93e860c5fc149a5c9adb35dd
Aanya Mishra <aa.nyamishra9503@gmail.com>
2026-06-02 18:56:49 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,16 +3,3 @@
 #table accounts
 #table transaction
 
-import mysql.connector as sql
-
-db = sql.connect(host="localhost",
-                 user="root",
-                 password="my25sqlroot2008",
-                 database="banksystem")
-
-cur = db.cursor()
-cur.execute(""" CREATE DATABASE banksystem
-""")
-cur.execute("""CREATE TABLE IF NOT EXIST user""")
-db.commit()
-print("Database connected and tables ready.")
