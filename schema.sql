a051423ad09daf76f8f35205baf5c550e9cf03a0
Aanya Mishra <aa.nyamishra9503@gmail.com>
2026-06-03 23:12:31 +0530

    Update schema create table accouns.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,11 @@
 CREATE DATABASE IF NOT EXISTS banking-system;
+USE banking-system;
+
+CREATE TABLE accounts(
+    account-id INT AUTO_INCREMEMT PRIMARY KEY,
+    fullname VARCHAR(25) NOT NULL,
+    email VARCHAR(25) UNIQUE,
+    phone INT UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    balance DECIMAL (12,2) DEFAULT 0.00,
+    created-at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
