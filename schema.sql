a768963d747c8c0e9c13c15525deba58168e1f1a
aanyamishra9503 <aa.nyamishra9503@gmail.com>
2026-06-04 23:08:21 +0530

    added new files and updates

--- schema.sql
+++ schema.sql
@@ -9,3 +9,14 @@ CREATE TABLE accounts(
     password VARCHAR(255) NOT NULL,
     balance DECIMAL (12,2) DEFAULT 0.00,
     created-at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+
+CREATE TABLE transactions (
+   transaction_id INT PRIMARY KEY AUTO_INCREMENT,
+   account_no INT,
+   transaction_type VARCHAR(20),
+   amount FLOAT,
+   transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  
+   FOREIGN KEY (account_no)
+   REFERENCES accounts(account_no)
+);
